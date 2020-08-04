2482a484db2c0773d2248a50afcef8ad8c2b94fb
Alonsogcl <62973685+Alonsogcl@users.noreply.github.com>
2020-08-03 22:21:34 -0500

    Updates

---
+++ precache-manifest.7c942eb8999f94f341c3e5dd0606ba1f.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d4719104a6225fe0ff006be97001b02",
+    "url": "/portafolio-personal/index.html"
+  },
+  {
+    "revision": "eaa1cc81b55465bc6e86",
+    "url": "/portafolio-personal/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "e0d935cf22aa09a383f5",
+    "url": "/portafolio-personal/static/css/main.c78a63a4.chunk.css"
+  },
+  {
+    "revision": "eaa1cc81b55465bc6e86",
+    "url": "/portafolio-personal/static/js/2.43e359ea.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/portafolio-personal/static/js/2.43e359ea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e0d935cf22aa09a383f5",
+    "url": "/portafolio-personal/static/js/main.42dd8578.chunk.js"
+  },
+  {
+    "revision": "547271df3c56dff4a043",
+    "url": "/portafolio-personal/static/js/runtime-main.4e6bc29e.js"
+  },
+  {
+    "revision": "c5d35a4a3cca5fe054174d99ff9ec2ec",
+    "url": "/portafolio-personal/static/media/carousel1.c5d35a4a.png"
+  },
+  {
+    "revision": "cc0c4bc456d3b6f1837429fe812f070b",
+    "url": "/portafolio-personal/static/media/carousel2.cc0c4bc4.png"
+  },
+  {
+    "revision": "a9f187956d62d62f799e351bb1849d80",
+    "url": "/portafolio-personal/static/media/carousel3.a9f18795.jpg"
+  },
+  {
+    "revision": "faa55ac49617ff60a38d55c5effafe1a",
+    "url": "/portafolio-personal/static/media/fondo1.faa55ac4.jpg"
+  },
+  {
+    "revision": "da86d697c4a08e04b06f8b0ca23909ae",
+    "url": "/portafolio-personal/static/media/logo-fedoers.da86d697.png"
+  },
+  {
+    "revision": "cf0de9323ae60763bf0be2cac79115e6",
+    "url": "/portafolio-personal/static/media/logo-xpectre.cf0de932.png"
+  },
+  {
+    "revision": "d3a1699754dffa2ba1fe3fd9fd621622",
+    "url": "/portafolio-personal/static/media/preview1.d3a16997.png"
+  },
+  {
+    "revision": "f2910f44a7e7a278f1bf8846d0025017",
+    "url": "/portafolio-personal/static/media/preview2.f2910f44.png"
+  },
+  {
+    "revision": "1b5cbfad6dd7eaa0895354942a9b6445",
+    "url": "/portafolio-personal/static/media/preview3.1b5cbfad.png"
+  }
+]);
